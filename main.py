2c5fba64a9efb29194f8ea121f210bf7490ba7d2
alejlatorre <alejandro.latorre@pucp.edu.pe>
2021-08-12 01:39:04 -0500

    modified files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# %% 0. Librerías
+ # %% 0. Librerias
 import pandas as pd 
 import numpy as np 
-from geopy.geocorders import Nominatim
+from geopy.geocoders import Nominatim
 
 # %% Datos
 in_path = 'data/in/'
@@ -10,5 +10,22 @@ out_path = 'data/out/'
 filename = 'train.csv'
 df = pd.read_csv(in_path + filename)
 
+df.columns = map(str.lower, df.columns)
+df.columns = map(str.strip, df.columns)
+
 # %% 
 geolocator = Nominatim(user_agent='scrapping_location')
+
+def get_region_from_geo(latitude, longitude):
+    try:
+        return geolocator.reverse(str(latitude) + ', ' + str(longitude)).raw['address']['state']
+    except:
+        return np.nan
+
+# %%
+df['region_origen'] = df.apply(lambda x: get_region_from_geo(str(x['latitud_origen']), str(x['longitud_origen'])), axis=1)
+df['region_destino'] = df.apply(lambda x: get_region_from_geo(str(x['latitud_destino']), str(x['longitud_destino'])), axis=1)
+
+# %%
+df.to_csv(out_path + 'file_with_region.csv')
+# %%
