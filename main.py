46bf0d102483702eb0e6686be0a2f196943de4c5
alejlatorre <alejandro.latorre@pucp.edu.pe>
2021-08-12 00:04:57 -0500

    added requirements file

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+# %% 0. Librerías
 import pandas as pd 
+import numpy as np 
+from geopy.geocorders import Nominatim
 
+# %% Datos
+in_path = 'data/in/'
+out_path = 'data/out/'
+
+filename = 'train.csv'
+df = pd.read_csv(in_path + filename)
+
+# %% 
+geolocator = Nominatim(user_agent='scrapping_location')
