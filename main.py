463e3a7f61bc781248b50bdc52f165507b81ea9f
markotalledo <marko.talledo@pucp.edu.pe>
2021-08-16 02:45:09 -0500

    Tidying

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 # %% 0. Librerias
+import geopy.distance
 import json
 import requests
 import numpy as np
@@ -11,8 +12,10 @@ from geopy.geocoders import Nominatim
 # %% Datos
 in_path = 'data/in/'
 out_path = 'data/out/'
-
 filename = 'train.csv'
+
+
+# %%
 df = pd.read_csv(in_path + filename)
 
 df.columns = map(str.lower, df.columns)
@@ -165,6 +168,17 @@ test_data.to_csv(out_path + 'test_with_states.csv')
 
 # %%
 
+
+test_data = pd.read_csv(out_path + 'test_with_states.csv')
+
+# %%
+test_data.head()
+
+# %%
+
+
+# %%
+
 list_dist = []
 list_times = []
 for i in test_data['id']:
@@ -174,28 +188,95 @@ for i in test_data['id']:
     lat_des = test_data.loc[test_data['id'] == i, 'latitud_destino'].values[0]
     lon_des = test_data.loc[test_data['id'] == i, 'longitud_destino'].values[0]
     # Uso de api
-    r = requests.get(
-        f"http://router.project-osrm.org/route/v1/car/{lon_or},{lat_or};{lon_des},{lat_des}?overview=false""")
-    rts = json.loads(r.content)
     try:
+        r = requests.get(
+            f"http://router.project-osrm.org/route/v1/car/{lon_or},{lat_or};{lon_des},{lat_des}?overview=false""")
+        rts = json.loads(r.content)
         route = rts.get("routes")[0]
         distancia = route['distance']
         tiempo = route['duration']
         list_dist.append(distancia)
         list_times.append(tiempo)
     except:
-        list_dist.append(0)
-        list_times.append(0)
-print(len(list_dist), len(list_times))
+        distancia = geopy.distance.distance(
+            (lat_or, lon_or), (lat_des, lon_des)).m
+        tiempo = distancia/(40000/3600)
+        list_dist.append(distancia)
+        list_times.append(tiempo)
 
 test_data['distancia_api'] = list_dist
 test_data['tiempo_api'] = list_times
 
 # %%
 
-test_data.loc[test_data['distancia_api'] == 0]
+test_data.loc[test_data['id'] == 124093563, 'distancia_api'] = geopy.distance.distance(
+    (-17.72164, -71.28891), (-17.7311, -71.29484)).m
+
+# %%
+test_data.loc[test_data['id'] == 124093563,
+              'tiempo_api'] = 1221.407916*3600/40000
+
+# %%
+
+# %%
+test_data.loc[test_data['id'] == 124093563, :]
 
 
 # %%
+
+
+# DROPEAR DIST EN KM
+# VERIFICAR SI AMBOS TIENEN LAS MISMAS CATEGORIAS EN COLUMNAS REGION ORIGGEN Y DESTINO
+# GET DUMMIES A AMBOS
+
+# %%
+
+df = pd.read_csv(out_path + 'file_cleaned.csv')
+df.head()
+
+
+# %%
+df.drop(columns=['distancia_km', 'Unnamed: 0', 'Unnamed: 0.1'], inplace=True)
+df.head()
+
+# %%
+
+test_data.drop(columns=['Unnamed: 0'], inplace=True)
+test_data.head()
+
+# %%
+test_data.to_csv(out_path + 'test_cleaned.csv')
+df.to_csv(out_path + 'file_cleaned.csv')
+
+
+# %%
+df = pd.get_dummies(df, columns=['region_origen', 'region_destino'])
+
+
+# %%
+test_data = pd.get_dummies(
+    test_data, columns=['region_origen', 'region_destino'])
+
+# %%
+
+
+# Get missing columns in the training test
+missing_cols = set(df.columns) - set(test_data.columns)
+# Add a missing column in test set with default value equal to 0
+for c in missing_cols:
+    test_data[c] = 0
+# Ensure the order of column in the test set is in the same order than in train set
+test_data = test_data[df.columns]
+
+# GET DUMMIES
+# AGREGAR COLUMNA CALLAO
+# ORDENAR COLUMNAS
+# %%
+df
+# %%
+
+test_data.drop(columns=['distancia', 'tiempo'], inplace=True)
+# %%
+df.to_csv(out_path + 'train_cleaned.csv')
 test_data.to_csv(out_path + 'test_cleaned.csv')
 # %%
